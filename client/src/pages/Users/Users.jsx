fd3b6c7b53daa9be86b0a3776ae119c9135704ce
Sahil Upadhyay <67515591+Sahil-Upadhyay@users.noreply.github.com>
2023-03-04 15:54:44 +0530

    Update Users.jsx

--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import './Users.css'
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import UsersList from './UsersList'
