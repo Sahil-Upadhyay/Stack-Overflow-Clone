f63847098bb1c30fe37e0c7383143d790b53b8a3
Sahil Upadhyay <67515591+Sahil-Upadhyay@users.noreply.github.com>
2023-03-12 20:29:15 +0530

    Update Questions.js

--- server/routes/Questions.js
+++ server/routes/Questions.js
@@ -9,6 +9,6 @@ const router = express.Router()
 router.post('/Ask', auth, AskQuestion)
 router.get('/get', getAllQuestions)
 router.delete('/delete/:id', auth, deleteQuestion );
-router.patch('/vote/:id', auth, voteQuestion)
+router.patch('/vote/:id', auth, voteQuestion);
 
 export default router
